63572a6a1bdef5fafecd4666af083589b9cd400d
xmyellow <286451004@qq.com>
2016-12-29 13:48:29 +0800

    创建一个vue+element的starter

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+  <myheader></myheader>
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="startHacking">Let's do it</el-button>
+<hello></hello>
+
+  </div>
+</template>
+
+<script>
+import myheader from './components/Nav.vue'
+import hello from './components/Hello.vue'
+export default {
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+  name: 'app',
+  components: {
+    myheader, hello
+  },
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It Works',
+        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+        duration: 6000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="myheader">
+  <el-menu default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">首页</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">作品</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+  </el-submenu>
+  <el-menu-item index="3">github</el-menu-item>
+ </el-menu>
+  </div>
+</template>
+<script>
+export default {
+  name: 'myheader',
+  methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
